f33fcfcc87ee8096b5baf94e7b6652be224f2367
pohzipohzi <poh.zihow@gmail.com>
2020-07-19 06:09:27 +0800

    add diff

--- internal/main.go
+++ internal/main.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"bytes"
 	"context"
 	"flag"
 	"fmt"
@@ -25,17 +26,16 @@ func Main() {
 	var (
 		help    bool
 		command string
+		diff    string
 	)
 	flag.BoolVar(&help, "h", false, "print usage")
 	flag.StringVar(&command, "c", "", "the command to run")
+	flag.StringVar(&diff, "d", "", "(optional) the command to diff against")
 	flag.Parse()
 	if help || command == "" {
 		flag.Usage()
 		return
 	}
-
-	args := strings.Split(command, " ")
-	cmdFactory := cmd.NewFactory(args[0], args[1:])
 
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	cmdStdinChan := make(chan string)
@@ -44,25 +44,64 @@ func Main() {
 		cancelFunc()
 	}()
 
+	var (
+		cmdFactory  cmd.Factory
+		diffFactory cmd.Factory
+	)
+	cmdArgs := strings.Split(command, " ")
+	cmdFactory = cmd.NewFactory(cmdArgs[0], cmdArgs[1:])
+	if diff != "" {
+		diffArgs := strings.Split(diff, " ")
+		diffFactory = cmd.NewFactory(diffArgs[0], diffArgs[1:])
+	}
+
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		case s := <-cmdStdinChan:
+			fmt.Fprintln(os.Stderr, "INPUT")
+			fmt.Fprint(os.Stderr, s)
+			b := []byte(s)
+
+			// run command
 			f, err := cmdFactory.New()
 			if err != nil {
 				fmt.Fprintln(os.Stderr, "Error creating command:", err)
 				continue
 			}
-			fmt.Fprintln(os.Stderr, "INPUT")
-			fmt.Fprint(os.Stderr, s)
-			res, err := f([]byte(s))
+			res, err := f(b)
 			if err != nil {
 				fmt.Fprintln(os.Stderr, "Error running command:", err)
 				continue
 			}
-			fmt.Fprintln(os.Stderr, "OUTPUT")
+			if diff == "" {
+				fmt.Fprintln(os.Stderr, "OUTPUT")
+				fmt.Fprint(os.Stdout, string(res))
+				continue
+			}
+
+			// run diff
+			f2, err := diffFactory.New()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error creating command:", err)
+				continue
+			}
+			res2, err := f2(b)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error running command:", err)
+				continue
+			}
+			if bytes.Compare(res, res2) == 0 {
+				fmt.Fprintln(os.Stderr, "EQUAL")
+				continue
+			}
+			fmt.Fprintln(os.Stdout, "NOT EQUAL")
+			fmt.Fprintln(os.Stderr, "Output for \""+command+"\"")
 			fmt.Fprint(os.Stdout, string(res))
+			fmt.Fprintln(os.Stderr, "Output for \""+diff+"\"")
+			fmt.Fprint(os.Stdout, string(res2))
+
 		}
 	}
 }
